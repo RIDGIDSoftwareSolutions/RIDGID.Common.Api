555adca0bdcad3ccb24f0dd731e316bb1550265a
DSeymour <devin.seymour@emerson.com>
2018-08-16 15:37:24 -0400

    added an iso 8601 datetime attribute

---
+++ src/RIDGID.Common.Api.Core.Tests/AttributesTests/RidgidIso8601DateTimeAttributeShould.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using NUnit.Framework;
+using RIDGID.Common.Api.Core.Attributes;
+using RIDGID.Common.Api.Core.Utilities;
+using Shouldly;
+
+namespace RIDGID.Common.Api.Core.Tests.AttributesTests
+{
+    internal class ModelWithDateTimeFieldWithoutCustomErrorMessage
+    {
+        [RidgidIso8601DateTime(1)] public string CreatedDate { get; set; }
+    }
+
+    internal class ModelWithDateTimeFieldWithCustomErrorMessage
+    {
+        [RidgidIso8601DateTime(1, "CustomMessage")]
+        public string CreatedDate { get; set; }
+    }
+
+    [TestFixture]
+    public class RidgidIso8601DateTimeAttributeShould
+    {
+        [Test]
+        public void InvalidateCorrectlyWithoutCustomErrorMessage()
+        {
+            //--Arrange
+            var model = new ModelWithDateTimeFieldWithoutCustomErrorMessage
+            {
+                CreatedDate = "invalidDate"
+            };
+            var validationContext = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            FormatResponseMessage.SetSnakeCaseSetting(false);
+
+            //--Act
+            var valid = Validator.TryValidateObject(model, validationContext, result, true);
+
+            //--Assert
+            valid.ShouldBeFalse();
+            result.Count.ShouldBe(1);
+            const string defaultErrorMsg = "The 'CreatedDate' must be in ISO-8601 format, (i.e. yyyy-mm-ddThh:mm:ss.ffffff).";
+            result[0].ErrorMessage
+                .ShouldBe(ModelStateCustomErrorMessage.Create(1, defaultErrorMsg));
+        }
+
+        [Test]
+        public void InvalidateCorrectlyWithCustomErrorMessage()
+        {
+            //--Arrange
+            var model = new ModelWithDateTimeFieldWithCustomErrorMessage
+            {
+                CreatedDate = "invalidDate"
+            };
+            var validationContext = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            //--Act
+            var valid = Validator.TryValidateObject(model, validationContext, result, true);
+
+            //--Assert
+            valid.ShouldBeFalse();
+            result.Count.ShouldBe(1);
+            result[0].ErrorMessage
+                .ShouldBe(ModelStateCustomErrorMessage.Create(1, "CustomMessage"));
+        }
+
+        [Test]
+        public void ValidateCorrectlyForDate()
+        {
+            //--Arrange
+            var model = new ModelWithDateTimeFieldWithoutCustomErrorMessage
+            {
+                CreatedDate = "2018-06-09T07:35:16Z"
+            };
+            var validationContext = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            //--Act
+            var valid = Validator.TryValidateObject(model, validationContext, result, true);
+
+            //--Assert
+            valid.ShouldBeTrue();
+            result.Count.ShouldBe(0);
+        }
+
+        [Test]
+        public void ValidateCorrectlyForOptionalDate()
+        {
+            //--Arrange
+            var model = new ModelWithDateTimeFieldWithoutCustomErrorMessage
+            {
+                CreatedDate = null
+            };
+            var validationContext = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            //--Act
+            var valid = Validator.TryValidateObject(model, validationContext, result, true);
+
+            //--Assert
+            valid.ShouldBeTrue();
+            result.Count.ShouldBe(0);
+        }
+
+        [Test]
+        public void ConvertFieldNameToSnakeCaseIfSetInAppConfig()
+        {
+            //--Arrange
+            var model = new ModelWithDateTimeFieldWithoutCustomErrorMessage
+            {
+                CreatedDate = "invalidDate"
+            };
+            var validationContext = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            FormatResponseMessage.SetSnakeCaseSetting(true);
+
+            //--Act
+            var valid = Validator.TryValidateObject(model, validationContext, result, true);
+
+            //--Assert
+            valid.ShouldBeFalse();
+            result.Count.ShouldBe(1);
+            const string defaultErrorMsg = "The 'created_date' must be in ISO-8601 format, (i.e. yyyy-mm-ddThh:mm:ss.ffffff).";
+            result[0].ErrorMessage
+                .ShouldBe(ModelStateCustomErrorMessage.Create(1, defaultErrorMsg));
+        }
+    }
+}
---
+++ src/RIDGID.Common.Api.Core/Attributes/RidgidISO8601DateTimeAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RIDGID.Common.Api.Core.Utilities;
+
+namespace RIDGID.Common.Api.Core.Attributes
+{
+    public class RidgidIso8601DateTimeAttribute : RidgidValidationAttribute
+    {
+        public const string DateTimeRegex =
+            "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\\.[0-9]+)?(Z)?$";
+
+        public RidgidIso8601DateTimeAttribute(int errorId) : base(errorId)
+        {
+        }
+
+        public RidgidIso8601DateTimeAttribute(int errorId, string customErrorMessage) : base(
+            errorId, customErrorMessage)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            return value == null || new RegularExpressionAttribute(DateTimeRegex).IsValid(value);
+        }
+
+        public override string FormatErrorMessage(string fieldName)
+        {
+            var errorMessage = CustomErrorMessage ?? CreateErrorMessage(fieldName);
+            return ModelStateCustomErrorMessage.Create(ErrorId, errorMessage);
+        }
+
+        private string CreateErrorMessage(string fieldName)
+        {
+            return $"The '{FormatResponseMessage.GetCasing(fieldName)}' must be in ISO-8601 format, (i.e. yyyy-mm-ddThh:mm:ss.ffffff).";
+        }
+    }
+}
