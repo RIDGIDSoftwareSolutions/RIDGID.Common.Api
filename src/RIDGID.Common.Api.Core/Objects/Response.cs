ce18b97cdfa00fd79cb4f124fdf80ffbe513bb2e
DSeymour <devin.seymour@emerson.com>
2019-04-10 14:41:24 -0400

    adding a generic response to wrap objects in a data envelope

---
+++ src/RIDGID.Common.Api.Core/Objects/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace RIDGID.Common.Api.Core.Objects
+{
+    public class Response<T>
+    {
+        public Response(T data)
+        {
+            Data = data;
+        }
+
+        public T Data { get; set; }
+    }
+}
